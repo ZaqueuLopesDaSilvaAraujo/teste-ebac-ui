15d0d0056e5ff2ec3bdb134704d2e468c3979a60
Zaqueu Araujo <zaqueu.tabata@gmail.com>
2025-04-29 00:44:26 -0300

    testes negativos

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,14 +1,40 @@
 
 ///<reference types="cypress"/>
 
+const { should } = require("chai");
+
 describe('Funcionalidade: Login', () => {
  
-    it('Deve fazer login com sucesso', () => {
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    }); 
+    
+     afterEach(() => {
+        cy.screenshot()
+     });
+
+    it('Deve fazer login com sucesso', () => {
+        
         cy.get('#username').type('zaqueu.tabata@gmail.com')
         cy.get('#password').type('Mudar@123')
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, zaqueu.tabata (não é zaqueu.tabata? Sair)')
+    });
+
+    it.only('Deve exibir mensagem de erro ao inserir usuário inválido', () => {
+
+        cy.get('#username').type('zaqueu@gmail.com')
+        cy.get('#password').type('Mudar@123')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('contain' , 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
+    });
+    
+    it.only('Deve exibir mensagem de erro ao inserir senha inválida', () => {
+
+        cy.get('#username').type('zaqueu.tabata@gmail.com')
+        cy.get('#password').type('Mudar@12')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('contain' , 'Erro: A senha fornecida para o e-mail zaqueu.tabata@gmail.com está incorreta. Perdeu a senha?')
     });
 
 });
